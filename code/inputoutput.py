f1a1f27252d5c2ee3957aa532bd83698b77d202c
Lee <Joseph.Lee@ec.gc.ca>
2025-07-28 11:59:01 -0400

    images, videos, webcam

--- code/inputoutput.py
+++ code/inputoutput.py
@@ -1,9 +1,15 @@
 import os
 import cv2
 
-# read images
+# read images / videos
 image_path = os.path.join('.', 'Resources', 'Photos', 'cat.jpg')
 img = cv2.imread(image_path)
+
+video_path = os.path.join('.', 'Resources', 'Videos', 'dog.mp4')
+video = cv2.VideoCapture(video_path)
+
+webcam = cv2.VideoCapture(0)
+
 
 # write images
 
@@ -13,5 +19,24 @@ cv2.imwrite(os.path.join('.', 'Resources', 'Photos', 'cat_out.jpg'), img)
 
 cv2.imshow('image', img)
 cv2.waitKey(0)
+'''
+ret = True
+while ret:
+    ret, frame = video.read()
+    cv2.imshow('video', frame)
 
+    if cv2.waitKey(1) == ord('q'):
+        break
  
+video.release()
+'''
+ret = True
+while True:
+    ret, frame = webcam.read()
+    cv2.imshow('webcam', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+webcam.release()
+
+cv2.destroyAllWindows()
