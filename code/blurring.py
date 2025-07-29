51cf33a53c54f9bea6f7afea6f4f062818d13cbd
Lee <Joseph.Lee@ec.gc.ca>
2025-07-29 11:46:20 -0400

    added comment

--- code/blurring.py
+++ code/blurring.py
@@ -6,7 +6,7 @@ img = cv.imread(os.path.join('.', 'Resources', 'Photos', 'lady.jpg'))
 k_size = 7 # the larger the value, the greater the blur
 img2 = cv.blur(img, (k_size, k_size))
 img3 = cv.GaussianBlur(img, (k_size, k_size), 3)
-img4 = cv.medianBlur(img, k_size)
+img4 = cv.medianBlur(img, k_size) # this type of blur can remove noise in the background which can be useful
 
 # cv.imshow('imgage', img)
 # cv.imshow('imgage', img2)
